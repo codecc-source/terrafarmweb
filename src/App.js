e5d617c7f5782407d93b45e912dbc49e52264111
codecc-source <carlitotingson@gmail.com>
2024-06-01 23:24:54 +0800

    Initial template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import backgroundImage from './images/backgroundImg.jpg'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <div className="logo">
+            <span>TERRA GRANDE FARM</span>
+          </div>
+          <ul className="nav-links">
+            <li><a href="#about">ABOUT US</a></li>
+            <li><a href="#products">PRODUCTS</a></li>
+            <li><a href="#contact">CONTACT US</a></li>
+          </ul>
+        </nav>
       </header>
+      <main>
+      <div className="bg-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
+          <div className="hero-text">
+            <h1>TERRA GRANDE FARM</h1>
+            <p style={{color: 'white', fontSize: '20px', fontFamily: 'Arial, sans-serif'}}>A 10-hectare Regenerative Goat Farm breeding top-quality upgraded and Purebred Anglo-Nubians.</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
